4db4c552bf0b30f6441b9027b8bb1b5d656c4292
Theegala  Yeswanth Karthik <125806731+yeswanthkarthik@users.noreply.github.com>
2025-02-24 18:57:47 +0530

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.calculator.calculator"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.calculator.calculator"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "2.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
